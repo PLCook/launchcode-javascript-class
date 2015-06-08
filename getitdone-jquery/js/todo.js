99d6f8482878f826c77e3273daea6a30c32571a2
edhky <edhky1@gmail.com>
2015-06-08 15:52:58 -0500

    almost finished

--- getitdone-jquery/js/todo.js
+++ getitdone-jquery/js/todo.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
-    $('button').on('click', function(addtask) {
+    $('#submit').on('click', function(addtask) {
         addtask.preventDefault();
         var newtask = $('#Task').val();
-        $('#todonew').append('<li>'+newtask+'</li>');
-        document.getElementById("Tasks").reset();
+        var newline = $('<li>'+newtask+'<span class="glyphicon glyphicon-ok-circle"></span><span class="glyphicon glyphicon-remove-circle"></span></li>');
+        //var finishline = newline;
+            $('#todonew').append(newline);
+            $('#Task').val('');
+
+            $('.glyphicon-ok-circle', newline).on('click', function() {
+        	    $('#done').append(newline);
+                $('.glyphicon-ok-circle', newline).remove();
+                $('.glyphicon-remove-circle', newline).remove();
+            });
+
+            $('.glyphicon-remove-circle', newline).on('click', function() {
+                $(newline).remove();
+            });
+
+            $('#delete').on('click', function() {
+                $(newline, ('#done')).remove();
+            });
+
     });
-    $(".one").click(function () {
-		$(this).hide();
-	});
+
 });
 
 
